e4b8e5ad13869bbf692470e03095eaa821c4e51c
Nicholas Huynh <nickhuynhq@gmail.com>
2022-10-09 12:28:59 -0400

    Fix bugs with drag and drop feature, and adjusted styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,37 +3,90 @@ import "./App.scss";
 import InputField from "./components/InputField/InputField";
 import TodoList from "./components/TodoList/TodoList";
 import { Todo } from "./models/model";
-import Logo from "./assets/logo/tasker_logo.png"
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
+import Logo from "./assets/logo/tasker_logo.png";
 
 const App: React.FC = () => {
-  !localStorage.todos &&  localStorage.setItem('todos', JSON.stringify([]))
+  !localStorage.todos && localStorage.setItem("todos", JSON.stringify([]));
   const [todo, setTodo] = useState<string>("");
-  const [todos, setTodos] = useState<Todo[]>( JSON.parse(localStorage.getItem("todos") || "" ));
-  
+  const [todos, setTodos] = useState<Todo[]>(
+    JSON.parse(localStorage.getItem("todos") || "")
+  );
+  const [completedTodos, setCompletedTodos] = useState<Todo[]>([]);
+
   const handleAddTodo = (e: React.FormEvent) => {
     e.preventDefault();
-
-    if (todo){
+    if (todo) {
       // Spread the current todos list and add a new todo object
-      setTodos([...todos, {id: Date.now() , todo, isDone: false}])
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
       // Reset the todo state to empty
-      setTodo("")
+      setTodo("");
     }
   };
 
-  useEffect(()=>{
-    localStorage.setItem('todos', JSON.stringify(todos))
-  }, [todos])
+  // Set and save to localStorage upon change
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  // Track the source and destination of the draggable component
+  const onDragEnd = (result: DropResult) => {
+    const { source, destination } = result;
+
+    if (!destination) return;
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    )
+      return;
+
+    let add, active = todos, complete = completedTodos;
+    
+    // Check the source
+    if(source.droppableId === "TodosList") {
+      // If todo card is in Active container, set add and remove it from Active
+      add = active[source.index];
+      active.splice(source.index, 1)
+    } else {
+      // If todo card is in Completed container, set add and remove it from Completed
+      add = complete[source.index]
+      complete.splice(source.index, 1)
+    }
+
+    // Check the destination
+    if(destination.droppableId === "TodosList") {
+      // Take add variable and splice it into the destination 
+      active.splice(destination.index, 0, add)
+    } else {
+            // Take add variable and splice it into the destination 
+      complete.splice(destination.index, 0, add)
+    }
+
+    // Set the states of the containers to the dragged variables
+    setCompletedTodos(complete);
+    setTodos(active);
+  };
 
   return (
-    <div className="App">
-      <div className="heading">
-        <img className="heading__logo" src={Logo} alt="Tasker Logo"/>
-        <h1 className="heading__title">Tasker.</h1>
-      </div>  
-      <InputField todo={todo} setTodo={setTodo} handleAddTodo={handleAddTodo} />
-      <TodoList todos={todos} setTodos={setTodos}/>
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="App">
+        <div className="heading">
+          <img className="heading__logo" src={Logo} alt="Tasker Logo" />
+          <h1 className="heading__title">Tasker.</h1>
+        </div>
+        <InputField
+          todo={todo}
+          setTodo={setTodo}
+          handleAddTodo={handleAddTodo}
+        />
+        <TodoList
+          todos={todos}
+          setTodos={setTodos}
+          completedTodos={completedTodos}
+          setCompletedTodos={setCompletedTodos}
+        />
+      </div>
+    </DragDropContext>
   );
 };
 
--- src/components/TodoCard/TodoCard.tsx
+++ src/components/TodoCard/TodoCard.tsx
@@ -3,14 +3,16 @@ import { Todo } from "../../models/model";
 import "./TodoCard.scss";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
+import { Draggable } from "react-beautiful-dnd";
 
 type Props = {
+  index: number;
   todo: Todo;
   todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 };
 
-const TodoCard = ({ todo, todos, setTodos }: Props) => {
+const TodoCard = ({ index, todo, todos, setTodos }: Props) => {
   const [edit, setEdit] = useState<boolean>(false);
   const [editTodo, setEditTodo] = useState<string>(todo.todo);
   const inputRef = useRef<HTMLInputElement>(null);
@@ -32,59 +34,64 @@ const TodoCard = ({ todo, todos, setTodos }: Props) => {
   const handleEdit = (e: React.FormEvent, id: number) => {
     e.preventDefault();
     // If todo id matches edited todo, set the todo to the edited text
-    setTodos(todos.map((todo)=> (
-        todo.id === id ? {...todo, todo: editTodo} : todo
-    )));
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, todo: editTodo } : todo))
+    );
     setEdit(false);
   };
 
   // Have reference be set on TodoCard input field when edit is changed
   useEffect(() => {
     inputRef.current?.focus();
-  }, [edit])
+  }, [edit]);
 
   return (
-    <form className="todos__card" onSubmit= {(e) => handleEdit(e, todo.id)}>
-      {
-        // If the todo card is being edited change the text to an input field
-        // Else, display the todo card text as is
-        edit ? (
-          <input
-            className="todos__card--edit"
-            value={editTodo}
-            onChange={(e) => setEditTodo(e.target.value)}
-          />
-        ) : // If the todo task is done, use the strikethrough tag for the card text,
-        // Else, render the normal todo card text
-        todo.isDone ? (
-          <s className="todos__card--text">{todo.todo}</s>
-        ) : (
-          <span className="todos__card--text">{todo.todo}</span>
-        )
-      }
+    <Draggable draggableId={todo.id.toString()} index={index}>
+      {(provided, snapshot) => (
+        <form className={`todos__card ${snapshot.isDragging ? "drag" : ""}`} onSubmit={(e) => handleEdit(e, todo.id)} {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+          {
+            // If the todo card is being edited change the text to an input field
+            // Else, display the todo card text as is
+            edit ? (
+              <input
+                className="todos__card--edit"
+                value={editTodo}
+                onChange={(e) => setEditTodo(e.target.value)}
+              />
+            ) : // If the todo task is done, use the strikethrough tag for the card text,
+            // Else, render the normal todo card text
+            todo.isDone ? (
+              <s className="todos__card--text">{todo.todo}</s>
+            ) : (
+              <span className="todos__card--text">{todo.todo}</span>
+            )
+          }
 
-      <div className="todos__card__icons">
-        <span
-          className="todos__card__icon"
-          onClick={() => {
-            // Switch between edit modes
-            if (!edit && !todo.isDone) {
-              setEdit(!edit);
-            }
-          }}
-        >
-          <AiFillEdit />
-        </span>
+          <div className="todos__card__icons">
+            <span
+              className="todos__card__icon"
+              onClick={() => {
+                // Switch between edit modes
+                if (!edit && !todo.isDone) {
+                  setEdit(!edit);
+                }
+              }}
+            >
+              <AiFillEdit />
+            </span>
 
-        <span className="todos__card__icon">
-          <AiFillDelete onClick={() => handleDelete(todo.id)} />
-        </span>
+            <span className="todos__card__icon">
+              <AiFillDelete onClick={() => handleDelete(todo.id)} />
+            </span>
 
-        <span className="todos__card__icon" onClick={() => handleDone(todo.id)}>
-          <MdDone />
-        </span>
-      </div>
-    </form>
+            <span className="todos__card__icon" onClick={() => handleDone(todo.id)}>
+              <MdDone />
+            </span>
+          </div>
+        </form>
+      )}
+    </Draggable>
+   
   );
 };
 
